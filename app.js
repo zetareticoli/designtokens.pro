21481c13004747ba24792ad7f13cb5b8ffb5d361
Francesco Improta <me@francescoimprota.com>
2024-02-23 07:54:11 +0100

    feat: new font and blocks animation

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var blocks = document.querySelectorAll('.block');
+  window.addEventListener('scroll', function() {
+    blocks.forEach(function(block) {
+      if (isElementInViewport(block)) {
+        block.classList.add('slide');
+      }
+    });
+  });
+});
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
